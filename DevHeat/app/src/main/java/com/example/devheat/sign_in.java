6107cec4f169293218da74218dbfdb790083a726
Jorge Varela Zamora <jotaaloud@gmail.com>
2024-11-23 21:17:34 +0100

    Functionality of data and storage added

--- DevHeat/app/src/main/java/com/example/devheat/sign_in.java
+++ DevHeat/app/src/main/java/com/example/devheat/sign_in.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +15,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.devheat.database.DatabaseHelper;
+
+import java.util.Collections;
+import java.util.List;
+
 public class sign_in extends AppCompatActivity {
 
+    //private DatabaseHelper dbHelper;
     private Button btnSignIn;
+    private EditText etUserName, etPassword;
+    private TextView textView3;
+    private String user, password;
+    private Bundle extras;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -26,15 +41,37 @@ public class sign_in extends AppCompatActivity {
             return insets;
         });
 
+        Intent intent = getIntent();
+
+        String userName = intent.getStringExtra("userName");
+        String userPassword = intent.getStringExtra("userPassword");
+
+        etUserName = findViewById(R.id.etUserName);
+        etPassword = findViewById(R.id.etPassword);
+
+
         btnSignIn = findViewById(R.id.btnSignIn);
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                user = etUserName.getText().toString();
+                password = etPassword.getText().toString();
 
-                Intent intent = new Intent(sign_in.this, template.class);
-                startActivity(intent);
+                if(user.equals(userName) && password.equals(userPassword)){
+                    Intent intent = new Intent(sign_in.this, template.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(sign_in.this, "Incorrect password or username.",Toast.LENGTH_SHORT).show();
+                }
             }
         });
+        textView3 = findViewById(R.id.textView3);
+        extras = getIntent().getExtras();
+
+        if(extras != null){
+            textView3.setText(userName);
+        }
+
 
     }
 
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
+
 # DevHeat
-Automates GitHub profile edition and creation of READMEs.
+
+Android app to automate GitHub profile edition and README creation.
+
+
+I used SQLite, SharedPreferences, Internal Storage, XML elements generation...
+
+There are 5 activities:
+
+## MainActivity
+
+Starts the <ins>SQLite database</ins> and loads the users into new <ins>XML Buttons</ins> with theirnames.
+
+When you click on the username, his name and his password are added to the 'login' activity via <ins>'putExtra'</ins> on it's intent.
+
+## New user
+
+You can create new users with a name and a password, and the users will be inserted into the SQLite database.
+
+## login
+
+Gets the extras setted on 'MainActivity' and sets the TextView welcoming the user. If the Username and his password are correct, the next activity will start.
+
+## Template
+
+It starts with one Spinner with several options to add to the Readme generatos (Titles, text, text with a link...)
+and a Button. If you click the button, it will generate another <ins>XML Spinner</ins>. You can generate countless Spinners.
+
+3 Buttons on bottom:
+
+ - bntClipoard: Saves the README into the clipboard with the ClipboardManager.
+ - btnClearOptions: Deletes the content of the readme by removing all views from the container.
+ - btnSave: saves the readme file into the internal storage with <ins>FileOutputStream</ins>, and lets the user choose between the predetermined path or other path, with an AlertDialog.
+
+## Menu
+
+2 Buttons:
+ - deleteSP: deletes the SharedPreferences.
+ - deleteDB: truncates the database, eliminates all the users.
+
+A Switch:
+
+Lets the user choose between the dark-mode or the light-mode, and stores the option at the <ins>SharedPreferences</ins>.
